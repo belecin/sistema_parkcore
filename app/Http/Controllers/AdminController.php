a484bdaf856965db2e1b2be5e6690479fb93a916
belecin <luzbelenmayu@gmail.com>
2025-12-09 22:00:21 -0500

    dasboard2

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cliente;
 use App\Models\Espacio;
+use App\Models\Tarifa;
+use App\Models\Ticket;
 use App\Models\User;
+use App\Models\Vehiculo;
 use Illuminate\Http\Request;
 use Spatie\Permission\Models\Role;
 
@@ -17,7 +21,16 @@ class AdminController extends Controller
         })-> withTrashed()->count();
 
         $total_espacios = Espacio::count();
+        $total_espacios_libres = Espacio::where('estado','libre')->count();
+        $total_espacios_ocupados = Espacio::where('estado','ocupado')->count();
+        $total_espacios_mantenimiento = Espacio::where('estado','mantenimiento')->count();
 
-        return view('admin.index',compact('total_roles','total_usuarios','total_espacios'));
+        $total_tarifas = Tarifa::count();
+        $total_clientes = Cliente::count();
+        $total_vehiculos = Vehiculo::count();
+        $total_tickets_activos = Ticket::where('estado_ticket','activo')->count();
+
+        return view('admin.index',compact('total_roles','total_usuarios','total_espacios','total_espacios_libres',
+        'total_espacios_ocupados','total_espacios_mantenimiento','total_tarifas','total_clientes','total_vehiculos','total_tickets_activos'));
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -24,6 +24,7 @@
         <div class="row">
             <div class="col-md-9">
                 <div class="row">
+
                     <div class="col-md-3 col-sm-6 col-12">
                         <div class="info-box">
                             <span class="info-box-icon bg-info">
@@ -58,7 +59,7 @@
                         <!-- /.info-box -->
                     </div>
 
-                    <div class="col-md-3 col-sm-6 col-12">
+                    <div class="col-md-6 col-sm-6 col-12">
                         <div class="info-box">
                             <span class="info-box-icon bg-info">
                                 <a href="{{ url('/admin/espacios') }}">
@@ -67,8 +68,77 @@
                             </span>
 
                             <div class="info-box-content">
-                                <span class="info-box-text">Espacios registrados</span>
-                                <span class="info-box-number">{{ $total_espacios}} Espacios</span>
+                                <span class="info-box-text">{{ $total_espacios}} Espacios registrados</span>
+                                <span class="info-box-number">{{ $total_espacios_libres}} libres | {{ $total_espacios_ocupados}} ocupados | 
+                                    {{ $total_espacios_mantenimiento}}  mantenimiento</span>
+                            </div>
+                            <!-- /.info-box-content -->
+                        </div>
+                        <!-- /.info-box -->
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-3 col-sm-6 col-12">
+                        <div class="info-box">
+                            <span class="info-box-icon bg-info">
+                                <a href="{{ url('/admin/tarifas') }}">
+                                    <img src="{{ url('/images/tarifa.gif') }}" width="100%" alt="">
+                                </a>
+                            </span>
+
+                            <div class="info-box-content">
+                                <span class="info-box-text">Tarifas registrados</span>
+                                <span class="info-box-number">{{ $total_tarifas }} tarifas</span>
+                            </div>
+                            <!-- /.info-box-content -->
+                        </div>
+                        <!-- /.info-box -->
+                    </div>
+
+                    <div class="col-md-3 col-sm-6 col-12">
+                        <div class="info-box">
+                            <span class="info-box-icon bg-info">
+                                <a href="{{ url('/admin/clientes') }}">
+                                    <img src="{{ url('/images/cliente.gif') }}" width="100%" alt="">
+                                </a>
+                            </span>
+
+                            <div class="info-box-content">
+                                <span class="info-box-text"> Clientes registrados</span>
+                                <span class="info-box-number">{{ $total_clientes }} clientes</span>
+                            </div>
+                            <!-- /.info-box-content -->
+                        </div>
+                        <!-- /.info-box -->
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-12">
+                        <div class="info-box">
+                            <span class="info-box-icon bg-info">
+                                <a href="{{ url('/admin/vehiculos') }}">
+                                    <img src="{{ url('/images/auto.gif') }}" width="100%" alt="">
+                                </a>
+                            </span>
+
+                            <div class="info-box-content">
+                                <span class="info-box-text"> Vehiculos registrados</span>
+                                <span class="info-box-number">{{ $total_vehiculos }} vehiculos</span>
+                            </div>
+                            <!-- /.info-box-content -->
+                        </div>
+                        <!-- /.info-box -->
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-12">
+                        <div class="info-box">
+                            <span class="info-box-icon bg-info">
+                                <a href="{{ url('/admin/tickets') }}">
+                                    <img src="{{ url('/images/boleto.gif') }}" width="100%" alt="">
+                                </a>
+                            </span>
+
+                            <div class="info-box-content">
+                                <span class="info-box-text"> Ticket activos</span>
+                                <span class="info-box-number">{{ $total_tickets_activos }} tickets</span>
                             </div>
                             <!-- /.info-box-content -->
                         </div>
@@ -77,6 +147,8 @@
                 </div>
             </div>
             <div class="col-md-3">
+                <h1 id="reloj-hora" class="text-center font-weight-bold"></h1>
+                <h5 id="reloj-fecha" class="text-center"></h5>
                 <div class="card card-outline card-secondary">
                 <div class="card-header">
                 <h3 class="card-title"><b>Calendario</b></h3>
@@ -134,5 +206,36 @@
             calendar.HTMLElement.style.maxWidth = '100%';
             calendar.init();
         });
+    </script>
+
+    <script>
+        function actualizarReloj() {
+            const d = new Date();
+            const dias = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+            const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+            const diaSemana = dias[d.getDay()];
+            const dia = d.getDate();
+            const mes = meses[d.getMonth()];
+            const ano = d.getFullYear();
+
+            let h = d.getHours();
+            let m = d.getMinutes();
+            let s = d.getSeconds();
+
+            // Convertir a formato de 12 horas y determinar AM/PM
+            let meridiano = h >= 12 ? 'PM' : 'AM';
+            h = h % 12;
+            h = h ? h : 12; // La hora '0' debe ser '12'
+
+            m = m < 10 ? '0' + m : m;
+            s = s < 10 ? '0' + s : s;
+
+            document.getElementById('reloj-fecha').innerHTML = `${diaSemana}, ${dia} de ${mes} de ${ano}`;
+            document.getElementById('reloj-hora').innerHTML = `${h}:${m}:${s} ${meridiano}`;
+        }
+
+        setInterval(actualizarReloj, 1000);
+        actualizarReloj();
     </script>
 @stop
